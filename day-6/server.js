6ea8518ae1c050b6d9812896c3cb1ac80fa2994b
AshishSharma975 <ashish8389sharma@gmail.com>
2026-02-01 21:06:48 +0530

    modified

--- day-6/server.js
+++ day-6/server.js
@@ -3,7 +3,7 @@ const app = require('./src/app');
 const mongoose = require('mongoose');
 
 function connectDB(){
-    mongoose.connect("mongodb+srv://ashish:soO0dwYjf4dVXV1N@cluster0.6fducnw.mongodb.net/day-6")
+    mongoose.connect("")
     .then(()=>{
         console.log("Connected to DB");
     })
