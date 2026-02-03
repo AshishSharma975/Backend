c831f2977591baa5f114c0009ef70eb619579551
AshishSharma975 <ashish8389sharma@gmail.com>
2026-02-03 11:57:55 +0530

    changes app.jsx

--- day-8/Frontend/src/App.jsx
+++ day-8/Frontend/src/App.jsx
@@ -1,48 +1,33 @@
-import React from 'react'
-import { useState } from 'react'
-import axios from "axios"
+import React from "react";
+import { useState, useEffect } from "react";
+import axios from "axios";
 const App = () => {
+  const [notes, setnotes] = useState([]);
 
-  const [notes, setnotes] = useState([
-    {
-    title:"test title 1",
-    description:"test description"
-  },
-    {
-    title:"test title 2",
-    description:"test description"
-  },
-    {
-    title:"test title 3",
-    description:"test description"
-  },
-    {
-    title:"test title 4",
-    description:"test description"
-  },
-])
+  function FetchNotes() {
+    axios.get("http://localhost:3000/notes").then((res) => {
+      setnotes(res.data.notes);
+    });
+  }
 
-axios.get('http://localhost:3000/notes')
-.then((res)=>{
-  setnotes(res.data.notes)
-})
+  useEffect(() => {
+    FetchNotes();
+  }, []);
+
   return (
     <>
       <div className="notes">
-        {
-          notes.map(note=>{
-            return <div className="note">
-          <h1>{note.title}</h1>
-          <p>{note.description}</p>
-        </div>
-          })
-        }
-        
+        {notes.map((note) => {
+          return (
+            <div className="note">
+              <h1>{note.title}</h1>
+              <p>{note.description}</p>
+            </div>
+          );
+        })}
       </div>
     </>
+  );
+};
 
-
-  )
-}
-
-export default App
+export default App;
