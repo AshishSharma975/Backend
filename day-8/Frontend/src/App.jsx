878e1e153d11c565a5625de99236b2af5e240e65
AshishSharma975 <ashish8389sharma@gmail.com>
2026-02-03 17:04:26 +0530

    fix bugs

--- day-8/Frontend/src/App.jsx
+++ day-8/Frontend/src/App.jsx
@@ -14,14 +14,54 @@ const App = () => {
     FetchNotes();
   }, []);
 
+  function HandleSubmit(e) {
+    e.preventDefault();
+
+    const { title, description } = e.target.elements;
+
+    console.log(title.value, description.value);
+
+    axios
+      .post("http://localhost:3000/notes", {
+        title: title.value,
+        description: description.value,
+      })
+      .then((res) => {
+        console.log(res.data);
+
+        FetchNotes();
+      });
+  }
+
+  function HandleDeleteNote(noteId) {
+    axios.delete("http://localhost:3000/notes/" + noteId)
+    .then((res) => {
+      console.log(res.data);
+      FetchNotes();
+    });
+  }
+
   return (
     <>
+      <form className="note-create-form" onSubmit={HandleSubmit}>
+        <input name="title" type="text" placeholder="Enter Title" />
+        <input name="description" type="text" placeholder="Enter Discription" />
+        <button>Create Note</button>
+      </form>
+
       <div className="notes">
         {notes.map((note) => {
           return (
             <div className="note">
               <h1>{note.title}</h1>
               <p>{note.description}</p>
+              <button
+                onClick={() => {
+                  HandleDeleteNote(note._id);
+                }}
+              >
+                delete
+              </button>
             </div>
           );
         })}
