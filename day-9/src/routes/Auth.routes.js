571babd0f69cd1978fcead54ad8570adf98af206
AshishSharma975 <ashish8389sharma@gmail.com>
2026-02-06 12:50:53 +0530

    change auth route folder

--- day-9/src/routes/Auth.routes.js
+++ day-9/src/routes/Auth.routes.js
@@ -1,21 +1,28 @@
-const express = require("express")
-const UserModel = require("../models/user.model")
+const express = require("express");
+const UserModel = require("../models/user.model");
 
-const AuthRouter = express.Router()
+const AuthRouter = express.Router();
 
-AuthRouter.post("/register", async (req, res)=>{
-    const {name, email, password} = req.body
+AuthRouter.post("/register", async (req, res) => {
+  const { name, email, password } = req.body;
 
-   const user = await UserModel.create({
-        name,email,password
-    })
+  const existingUser = await UserModel.findOne({ email });
+  if (existingUser) {
+    return res.status(400).json({
+      message: "User with this email already exists",
+    });
+  }
 
-    res.status(201).json({
-        message:"user registered",
-        user
-    })
+  const user = await UserModel.create({
+    name,
+    email,
+    password,
+  });
 
-})
+  res.status(201).json({
+    message: "user registered",
+    user,
+  });
+});
 
-
-module.exports = AuthRouter
+module.exports = AuthRouter;
