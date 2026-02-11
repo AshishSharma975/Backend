a192dad57e20ab0c1afd4572f008d6305349d508
AshishSharma975 <ashish8389sharma@gmail.com>
2026-02-11 19:23:32 +0530

    set all register method

--- project-1/src/app.js
+++ project-1/src/app.js
@@ -1,9 +1,14 @@
 const express =  require("express")
-
+const cookieParser = require("cookie-parser")
 const app = express()
+const authRouter = require("../src/routes/auth.routes")
 const connectDB = require("./config/databse")
 app.use(express.json())
+app.use(cookieParser())
 
 
+
+
+app.use("/api/auth",authRouter)
 connectDB()
 module.exports = app
--- project-1/src/routes/auth.routes.js
+++ project-1/src/routes/auth.routes.js
@@ -2,6 +2,9 @@ const express = require("express")
 const userModel = require("../models/user.model")
 
 const authRouter = express.Router()
+const crypto = require("crypto")
+const jwt = require("jsonwebtoken")
+
 
 
 authRouter.post("/register", async (req,res)=>{
@@ -35,8 +38,43 @@ authRouter.post("/register", async (req,res)=>{
     })
     if(isUserAlreadyExist){
         return res.status(409).json({
-            message:"user already exist"
+            message:"user already exist" + (isUserAlreadyExist).email == email ? "email already exist": "username already exist"
         })
     }
 
+    const hash = crypto.createHash("md5").update(password).digest("hex")
+
+    const user = await userModel.create({
+        username,
+        password:hash,
+        email,
+        bio,
+        profileImage
+    })
+
+    const token = jwt.sign(
+        {
+        id:user._id
+       },
+       process.env.JWT_SECRET,{
+        expiresIn:"1d"
+       })
+    res.cookie("token", token)
+
+    res.status(201).json({
+        message:"user registered successfully",
+        user:{
+          email:user.email,
+          username:user.username,
+          bio:user.bio,
+          profileImage:user.profileImage
+        }
+    })
 })
+
+
+authRouter.post("/login",async (req,res)=>{
+    
+})
+
+module.exports = authRouter
