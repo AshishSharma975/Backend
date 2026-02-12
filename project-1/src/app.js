6e1af29a87a840248e68073da3a8a9cd62f110b9
AshishSharma975 <ashish8389sharma@gmail.com>
2026-02-12 19:10:38 +0530

    multer

--- project-1/src/app.js
+++ project-1/src/app.js
@@ -1,14 +1,23 @@
 const express =  require("express")
+
 const cookieParser = require("cookie-parser")
+
 const app = express()
+
 const authRouter = require("../src/routes/auth.routes")
+
 const connectDB = require("./config/databse")
+
+const postRouter = require("./routes/post.routes")
+
 app.use(express.json())
 app.use(cookieParser())
 
 
 
 
+
+app.use("/api/post", postRouter)
 app.use("/api/auth",authRouter)
 connectDB()
 module.exports = app
