31e75f4e27a15a4db7bf9139588b22a84a6cbe8c
AshishSharma975 <ashish8389sharma@gmail.com>
2026-02-15 18:52:17 +0530

    posts

--- project-1/src/routes/post.routes.js
+++ project-1/src/routes/post.routes.js
@@ -1,12 +1,46 @@
 const express = require("express")
-const createPostController = require("../controllers/post.controller")
+const { createPostController, getPostController } = require("../controllers/post.controller")
 const postRouter = express.Router()
 const multer = require("multer")
+const postModel = require("../models/post.model")
 const upload = multer({storage:multer.memoryStorage()})
 
 
 postRouter.post("/", upload.single("image"),createPostController)
 
 
-postRouter.get("/", postController)
+postRouter.get("/", getPostController)
+
+
+async function getPostDetailsController(req, res){
+    const token = req.cookies.token;
+
+    if(!token){
+        return res.status(401).json({
+            message: "anauthorized access.",
+        })
+    }
+
+    let decoded = null;
+
+    try{
+        decoded = jwt.verify(token, process.env.JWT_SECRET)
+    }catch(err){
+        return res.status(401).json({
+            message: "invalid token.",
+        })
+    }
+
+    const userId = decoded.id;
+
+    const postId = req.params.postId;
+
+    const post = await postModel.findById(postId)
+
+    if(post){
+        return res.status(404).json({
+            message:"post not found."
+        })
+    }
+}
 module.exports = postRouter
