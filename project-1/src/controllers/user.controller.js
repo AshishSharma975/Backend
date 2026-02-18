27bb61d31dfd11a3eb4db94d6417b83fcb0ed015
AshishSharma975 <ashish8389sharma@gmail.com>
2026-02-18 20:01:14 +0530

    follow-followinf-fix users

--- project-1/src/controllers/user.controller.js
+++ project-1/src/controllers/user.controller.js
@@ -6,6 +6,24 @@ async function followUserController(req,res) {
     const followingUsername = req.params.username
 
 
+    if(followingUsername == followerUsername){
+        return res.status(400).json({
+            message:"you cannot follow yourself."
+        })
+    }  
+
+    const isAlreadyFollowing = await followModel.findOne({
+     follower:followerUsername,
+     following:followerUsername
+    })
+
+    if(isAlreadyFollowing){
+        return res.status(200).json({
+            message:"you are already follwing.",
+            follow :isAlreadyFollowing
+        })
+    }
+
    const followRecord = await followModel.create({
     follower:followerUsername,
     following:followerUsername
