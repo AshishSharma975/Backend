2557b5af3eb355d2cd6eb1b21b1a888f2f06854c
AshishSharma975 <ashish8389sharma@gmail.com>
2026-02-22 19:44:28 +0530

    feat: Initialize Express application with essential middleware, routes, and database connection.

--- project-1/Backend/src/app.js
+++ project-1/Backend/src/app.js
@@ -11,20 +11,27 @@ app.use(cookieParser())
 app.use(cors({
     origin:"http://localhost:5173",
     credentials:true
+
 }))
 
 // require routes
 const authRouter = require("./routes/auth.routes")
+
 const postRouter = require("./routes/post.routes")
+
 const userRouter = require("./routes/user.routes")
+
 const followRouter = require("./routes/follow.routes")
 
 
 
 // using routes
 app.use("/api/post", postRouter)
+
 app.use("/api/auth",authRouter)
+
 app.use("/api/users",userRouter)
+
 app.use("/api/follow",followRouter)
 
 
