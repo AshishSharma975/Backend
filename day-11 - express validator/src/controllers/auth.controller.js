4035311b71b2c4c9607f34bfd074cc9dc44663c7
AshishSharma975 <ashish8389sharma@gmail.com>
2026-03-08 14:21:09 +0530

    handle-error

--- day-11 - express validator/src/controllers/auth.controller.js
+++ day-11 - express validator/src/controllers/auth.controller.js
@@ -1,5 +1,18 @@
-export async function registerUser(req,res) {
-    
-    throw new Error("encounter an erroe while registering new user.")
+export async function registerUser(req, res, next) {
+  //   try {
+  //     throw new Error("encounter an erroe while registering new user.");
+  //   } catch (err) {
+  //     next(err);
+  //   }
 
+  //   const err = new Error("password is too weak.");
+  //   err.status = 400;
+  //   next(err);
+
+  try {
+    throw new Error("password is too weak.");
+  } catch (err) {
+    err.status = 400;
+    next(err);
+  }
 }
---
+++ day-11 - express validator/src/middleware/error.middleware.js
@@ -0,0 +1,8 @@
+
+function handleError(err,req,res,next){
+    res.status(err.status).json({
+        message:err.message
+    })
+}
+
+export default handleError
